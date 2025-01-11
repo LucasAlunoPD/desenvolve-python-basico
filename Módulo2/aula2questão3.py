25f44d6e9996a17075413c33431021d07641c652
Lucas Macieira F M <lucas.menezes@pditabira.com>
2025-01-11 15:13:59 -0300

    Add files via upload

---
+++ Módulo2/aula2questão3.py
@@ -0,0 +1,18 @@
+
+v1 = 10
+v2 = 20
+
+
+print("Antes da troca:")
+print("v1 =", v1)
+print("v2 =", v2)
+
+
+x = v1  
+v1 = v2   
+v2 = x  
+
+
+print("Depois da troca:")
+print("v1 =", v1)
+print("v2 =", v2)
